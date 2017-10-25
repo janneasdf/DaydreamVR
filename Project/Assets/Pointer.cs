5ee917e2e4c64278aba52588a4a9aebe495d440f
Janne Gröndahl <janne.grondahl@aalto.fi>
2017-10-25 04:47:37 +0300

    Implement part 1

---
+++ Project/Assets/Pointer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pointer : MonoBehaviour {
+
+    public GameObject target;
+    public Vector3 hitPoint;
+    public GameObject pointerDot;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.rotation = GvrControllerInput.Orientation;
+
+        var origin = transform.position;
+        var direction = transform.forward;
+        RaycastHit hitInfo;
+        var hit = Physics.Raycast(origin, direction, out hitInfo);
+        Debug.DrawRay(origin, direction, Color.red);
+        if (hit)
+        {
+            target = hitInfo.transform.gameObject;
+            hitPoint = hitInfo.point;
+            pointerDot.transform.position = hitPoint;
+            pointerDot.SetActive(true);
+        }
+        else
+        {
+            target = null;
+            pointerDot.SetActive(false);
+        }
+	}
+}
