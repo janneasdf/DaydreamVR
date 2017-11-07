3f85fafc60893d520c67757280c31b360e85fb66
Janne Gröndahl <janne.grondahl@aalto.fi>
2017-11-07 07:19:02 +0200

    Work on image picker

--- Project/Assets/Scripts/PhotoPicker.cs
+++ Project/Assets/Scripts/PhotoPicker.cs
@@ -4,16 +4,27 @@ using UnityEngine;
 
 public class PhotoPicker : MonoBehaviour {
 
-    // Object which we'll put the chosen image to
+    [Tooltip("Object which we'll put the chosen image to.")]
     public GameObject photoHolder;
+
+    [Tooltip("Parent for the images to be shown.")]
+    public GameObject contentHolder;
+
+    [Tooltip("Prefab for the image objects in the scrollView.")]
+    public GameObject uiImagePrefab;
 
 	// Use this for initialization
 	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+        var photoPaths = Utilities.GetAllGalleryImagePaths().GetRange(0, 6);
+        Debug.Log(string.Join(",", photoPaths.ToArray()));
+        foreach (var path in photoPaths)
+        {
+            var imageObject = Instantiate(uiImagePrefab) as GameObject;
+            imageObject.transform.SetParent(contentHolder.transform);
+            var image = imageObject.GetComponent<UnityEngine.UI.RawImage>();
+            image.texture = Utilities.LoadImageAsTexture(path);
+            Debug.Log("Set texture :" + path);
+            Debug.Log(image.texture.ToString());
+        }
 	}
 }
