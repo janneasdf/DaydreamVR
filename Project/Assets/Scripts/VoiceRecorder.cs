a5d286f1fbcf2b80f38c6d41c7f3afa57c53740e
luckymj <ryuminjung@gmail.com>
2017-11-11 00:56:40 +0200

    Unused assets deleted

---
+++ Project/Assets/Scripts/VoiceRecorder.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class VoiceRecorder : MonoBehaviour {
+
+    private AudioSource microphoneAudioSource;
+
+    void Start()
+    {
+        microphoneAudioSource = transform.Find("Audio Source").gameObject.GetComponent<AudioSource>();
+    }
+
+    public void StartRecord()
+    {
+        microphoneAudioSource.clip = Microphone.Start(null, true, 10, 16000);
+        microphoneAudioSource.loop = false;
+        Debug.Log("StartRecord");
+    }
+
+    public void StopRecord()
+    {
+        int recordingPosition = Microphone.GetPosition(null); // do before calling end!
+        Microphone.End(null);
+        Debug.Log("StopRecord");
+    }
+
+    public void PlayRecord()
+    {
+        microphoneAudioSource.Play();
+        Debug.Log("PlayRecord");
+    }
+}
