edacc49a060a728ba5edb20417743f0e71f4fd11
Janne Gröndahl <janne.grondahl@aalto.fi>
2017-11-12 18:16:47 +0200

    Remove some unnecessary photo assets and work on DnD

--- Project/Assets/Scripts/State.cs
+++ Project/Assets/Scripts/State.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class State : MonoBehaviour {
-
+    [Tooltip("For easy access to photo picker, even if it is inactive. ")]
     public GameObject PhotoPicker;
+
+
 }
