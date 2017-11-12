e3882c7540201380ecd0310b40b78037ec7387c2
luckymj <ryuminjung@gmail.com>
2017-11-13 01:14:54 +0200

    Main menu added

--- Project/Assets/Scripts/VoiceRecorder.cs
+++ Project/Assets/Scripts/VoiceRecorder.cs
@@ -8,19 +8,31 @@ public class VoiceRecorder : MonoBehaviour {
 
     private AudioSource microphoneAudioSource;
     private Text recordingText;
+    private Text playingText;
+    private Button recordingButton;
+    private Button playingButton;
     private int recordingFlag; // True: recording on
+    private int playingFlag; // True: playing on
 
 
     void Start()
     {
         microphoneAudioSource = transform.Find("Audio Source").gameObject.GetComponent<AudioSource>();
-        recordingText = transform.Find("ButtonRecord").gameObject.GetComponent<Button>().transform.Find("RecordingText").gameObject.GetComponent<Text>();
+        recordingButton = transform.Find("ButtonRecord").gameObject.GetComponent<Button>();
+        recordingText = recordingButton.transform.Find("RecordingText").gameObject.GetComponent<Text>();
         recordingText.enabled = false;
+        playingButton = transform.Find("ButtonPlay").gameObject.GetComponent<Button>();
+        playingText = playingButton.transform.Find("PlayingText").gameObject.GetComponent<Text>();
+        playingText.enabled = false;
         recordingFlag = 0;
+        playingFlag = 0;
     }
 
     public void StartRecord()
     {
+        microphoneAudioSource.Stop();
+        playingFlag = 0;
+
         if ( recordingFlag == 1 )
         {
             int recordingPosition = Microphone.GetPosition(null); // do before calling end!
@@ -41,7 +53,30 @@ public class VoiceRecorder : MonoBehaviour {
 
     public void PlayRecord()
     {
-        microphoneAudioSource.Play();
-        Debug.Log("PlayRecord");
+        if ( playingFlag == 1 )
+        {
+            microphoneAudioSource.Stop();
+            playingFlag = 0;
+        }
+        else
+        {
+            if ( recordingFlag == 0 )
+            {
+                microphoneAudioSource.Play();
+                Debug.Log("PlayRecord");
+                playingText.enabled = true;
+                playingFlag = 1;
+            }
+        }
+    
+    }
+
+    void Update()
+    {
+        if (!microphoneAudioSource.isPlaying)
+        {
+            playingText.enabled = false;
+            playingFlag = 0;
+        }
     }
 }
