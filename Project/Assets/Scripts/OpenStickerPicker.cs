cef4a066434cd8f45ba7b4550f75238fa5c10eff
Janne Gröndahl <janne.grondahl@aalto.fi>
2017-11-13 06:22:03 +0200

    Disable Drag and Drop from ScenePhoto prefab

---
+++ Project/Assets/Scripts/OpenStickerPicker.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenStickerPicker : MonoBehaviour {
+
+	public void Open()
+    {
+        // Move image picker to be child of this game object
+        var photoPicker = Utilities.GetState().StickerPicker;
+        photoPicker.transform.SetParent(transform, false);
+        photoPicker.transform.SetAsLastSibling();
+        transform.SetAsLastSibling();
+        photoPicker.transform.localPosition = photoPicker.transform.localPosition + 50.0f * Vector3.up;
+
+        // Set the photo holder that will display the picked photo
+        //var pickerComponent = photoPicker.GetComponent<PhotoPicker>();
+        //var debugPickerComponent = photoPicker.GetComponent<DebugPhotoPicker>();
+        //if (pickerComponent) { pickerComponent.photoHolder = gameObject; }
+        //else { debugPickerComponent.photoHolder = gameObject; }
+
+        // Set picker active in case it was inactive
+        photoPicker.SetActive(true);
+    }
+}
