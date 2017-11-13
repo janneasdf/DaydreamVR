c5a8ffe09426e53b6d44c0ddfcf4ce4e04aa3f0a
luckymj <ryuminjung@gmail.com>
2017-11-13 03:44:26 +0200

    Snow terrain added

---
+++ Project/Assets/Scripts/SkyboxSetting.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyboxSetting : MonoBehaviour {
+
+    public Material newSky;
+
+	// Use this for initialization
+	void Start () {
+        RenderSettings.skybox = newSky;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
